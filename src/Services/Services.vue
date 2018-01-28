1c6c4a692131b260205a5c59db68798744998d93
AlexChe <thenorth@bk.ru>
2018-01-28 21:45:09 +0300

    add viewport metatag

---
+++ src/Services/Services.vue
@@ -0,0 +1,172 @@
+<template>
+    <div>
+        <div class="services">
+            <div class="serveces__title">Сервисы и лицензии</div>
+            <div class="serveces__checkbox"></div>
+
+            <div v-for="(license,index) in licenses">
+                <input
+                        type='checkbox'
+                        class="ios8-switch ios8-switch-sm "
+                        v-bind:id="'checkbox-' + index"
+                        v-model="checkedLicense"
+                        v-bind:value="license.price"
+                >
+                <label  v-bind:for="'checkbox-' + index">{{license.name}}</label>
+            </div>
+
+            <span>{{selectedLicense()}}</span>
+        </div>
+
+    </div>
+</template>
+
+
+
+<script>
+
+    export default {
+        data () {
+            return {
+
+                licenses: [
+                    {
+                        id: 1,
+                        name: 'Microsoft Windows',
+                        price: 1300,
+                    },
+//                    {
+//                        id: 2,
+//                        name: 'Linux',
+//                        price: 150,
+//
+//                    },
+//                    {
+//                        id: 3,
+//                        name: 'Ubuntu',
+//                        price: 50,
+//
+//                    },
+//                    {
+//                        id: 4,
+//                        name: 'Mint',
+//                        price: 250,
+//
+//                    },
+                ],
+                checkedLicense: []
+            }
+        },
+        methods:{
+            selectedLicense(){
+
+                let sum=0;
+                for(let i=0;i<this.checkedLicense.length;i++){
+                    sum = sum + parseInt(this.checkedLicense[i]);
+                }
+                this.$emit('selectedLicense',sum)
+            }
+        }
+
+    }
+    
+    
+</script>
+
+
+<style>
+    /*services*/
+    .services{
+        background: #eee;
+        margin: 20px;
+        padding: 15px;
+        border-radius: 10px;
+    }
+    .serveces__title{
+        font-size:18px;
+        margin-bottom:10px;
+        /*padding:10px;*/
+    }
+
+
+
+    /* КНОПКА КАК в АЙФОНЕ*/
+
+    input[type="checkbox"].ios8-switch {
+        position: absolute;
+        margin: 8px 0 0 16px;
+        display: none;
+    }
+    input[type="checkbox"].ios8-switch + label {
+        position:relative;
+        padding: 5px 0 0 50px;
+        line-height: 2.0em;
+    }
+    input[type="checkbox"].ios8-switch + label:before {
+        content: "";
+        position: absolute;
+        display: block;
+        left: -1px;
+        top: 0;
+        width: 40px; /* x*5 */
+        height: 24px; /* x*3 */
+        border-radius: 16px; /* x*2 */
+        background: #fff;
+        border: 1px solid #d9d9d9;
+        -webkit-transition: all 0.3s;
+        transition: all 0.3s;
+    }
+    input[type="checkbox"].ios8-switch + label:after {
+        content: "";
+        position: absolute;
+        display: block;
+        left: 0px;
+        top: 2px;
+        width: 24px; /* x*3 */
+        height: 24px; /* x*3 */
+        border-radius: 16px; /* x*2 */
+        background: #fff;
+        border: 1px solid #d9d9d9;
+        -webkit-transition: all 0.3s;
+        transition: all 0.3s;
+    }
+    input[type="checkbox"].ios8-switch + label:hover:after {
+        box-shadow: 0 0 5px rgba(0,0,0,0.3);
+    }
+    input[type="checkbox"].ios8-switch:checked + label:after {
+        margin-left: 16px;
+    }
+    input[type="checkbox"].ios8-switch:checked + label:before {
+        background: #B44536;
+    }
+
+    /* SMALL */
+
+    input[type="checkbox"].ios8-switch-sm {
+        margin: 5px 0 0 10px;
+    }
+    input[type="checkbox"].ios8-switch-sm + label {
+        position: relative;
+        padding: 0 0 0 32px;
+        line-height: 1.3em;
+    }
+    input[type="checkbox"].ios8-switch-sm + label:before {
+        width: 25px; /* x*5 */
+        height: 17px; /* x*3 */
+        border-radius: 10px; /* x*2 */
+    }
+    input[type="checkbox"].ios8-switch-sm + label:after {
+        width: 13px; /* x*3 */
+        height: 13px; /* x*3 */
+        border-radius: 10px; /* x*2 */
+    }
+    input[type="checkbox"].ios8-switch-sm + label:hover:after {
+        box-shadow: 0 0 3px rgba(0,0,0,0.3);
+    }
+    input[type="checkbox"].ios8-switch-sm:checked + label:after {
+        margin-left: 10px; /* x*2 */
+    }
+
+
+
+</style>
---
+++ src/Tariff/Tariff.vue
@@ -0,0 +1,123 @@
+<template>
+    <div>
+
+        <div class="tariff">
+            <div class="tariff__title">Тарифы</div>
+            <div class="tariff__item"
+                 @click.prevent="activeItem = 1"
+                 :class="{ tariff__item_active: activeItem === 1}"
+            >
+                <div class="tariff__name">Гарантированный</div>
+                <div class="tariff__price tariff__price-rub">{{resTariff(tariff1)}} <span class="rub_25 rub-color-black">Р</span></div>
+                <div class="tariff__description">в месяц</div>
+            </div>
+
+            <div class="tariff__item"
+                 @click.prevent="activeItem = 2"
+                 :class="{ tariff__item_active: activeItem === 2}"
+            >
+                <div class="tariff__name">Гибкий</div>
+                <div class="tariff__price tariff__price-rub">{{resTariff(tariff2)}} <span class="rub_25 rub-color-black">Р</span></div>
+                <div class="tariff__description">в месяц</div>
+            </div>
+
+
+        </div>
+
+    </div>
+</template>
+
+
+
+<script>
+
+    export default {
+        props:{
+
+            tariff1: {
+                type: Number,
+                required: true,
+            },
+            tariff2: {
+                type: Number,
+                required: true,
+            },
+        },
+        data () {
+            return {
+                activeItem: null,
+            }
+        },
+        methods:{
+            resTariff(val){
+                return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+            }
+        }
+
+    }
+</script>
+
+
+<style>
+    /*tariff__item*/
+    .tariff__item{
+        display: inline-block;
+        width:250px;
+        border:1px solid #ccc;
+        text-align:left;
+        box-shadow: -1px 9px 13px 4px #a09999;
+        padding: 10px;
+        margin: 5px;
+
+    }
+    .tariff__price{
+        font-size:30px;
+    }
+    .tariff__description{
+        color:#b1aeae;
+        font-size: 12px;
+    }
+    .tariff__item:hover{
+        cursor:pointer;
+        opacity:0.7;
+    }
+    .tariff__item_active{
+        background: #e0d7d7;
+    }
+
+
+
+
+    /**/
+    .rub_25 {
+        font-family: arial;
+        font-size: 25px !important;
+        display: inline-block;
+        font-weight: 100;
+    }
+    .rub-color-black:before, .rub-color-black:after, .rub-color-black {
+        border-color: #000 !important;
+        color: #000 !important;
+    }
+
+    .rub_25:before, .rub_25:after {
+        content: "";
+        display: block;
+        border-bottom: 2px solid #000;
+        position: relative;
+        left: -1px;
+    }
+
+
+    .rub_25:before {
+        width: 8px;
+        top: 16px;
+    }
+
+    .rub_25:after {
+        width: 11px;
+        bottom: 11px;
+    }
+
+
+</style>
